6ed7fc81623ca66df14f906e8e0af6285b5b86ee
Devon Moubry <devon@moubry.com>
2017-08-19 16:50:26 -0500

    Add TODOs

--- app.js
+++ app.js
@@ -27,6 +27,28 @@ app.get('/', function (req, res) {
   res.render('index');
 })
 
+// TODO: Render validation error messages
+// TODO: display error message: input is invalid, please try again. your guess must be 1 letter.
+// TODO: if input is valid...
+// TODO: check to see if guess matches letter in guessed array
+// TODO: if guess matches letter in guessed array display message: you already guess that, please try again.
+// TODO: check to see if input matches letter in session
+// TODO: if guess matches letter in mystery word display user message
+// TODO: rerender home with usermessage and updated game play board
+// TODO: user message: _ is in the mystery word
+// TODO: letter added to guessed array?
+// TODO: if guess does not match letter in mystery word rerender page
+// TODO: subtract 1 from guesses count
+// TODO: add letter to guessed array
+// TODO: check to see if user guesses the word
+// TODO: if word is guess display You did it!
+// TODO: check to see if user ran out of turns
+// TODO: if user ran out of turns display You ran out of guesses.
+// TODO: display full word from session
+// TODO: tell computer gameplay = false
+// TODO: if gameplay is false display button with message do you want to play again?
+// TODO: if play again button is pressed page reloads with: new word, guess count 8, letters guessed array [], gamplay is true, new gameplay
+
 app.listen(3000, function () {
-  console.log('App listening on port 3000!');
+  console.log('🍸  Party at http://localhost:3000...');
 });
